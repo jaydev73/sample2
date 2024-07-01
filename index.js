761311bc955278d5b8b311f1bd35bf190022e6bf
Jay <butchich73@gmail.com>
2024-07-01 12:30:59 -0700

    Second commit

--- index.js
+++ index.js
@@ -14,5 +14,22 @@ function doSomething() {
     alert("I was clicked")
 }
 
-const button = document.getElementById("my-id");
-button.addEventListener("click", doSomething)
+// const button = document.getElementById("my-id");
+// button.addEventListener("click", doSomething)
+
+
+
+var today = new Date();
+var hournow = today.getHours();
+var  greeting;
+
+if(hournow > 18){
+    greeting = "Good Evening";
+}else if (hournow > 12) {
+    greeting = "Good Afternoon";
+}else if (hournow > 0) {
+    greeting = "Good Morning";
+}else {
+    greeting =  "Welcome";
+}
+document.write('<h3>' + greeting + '</h3>')
